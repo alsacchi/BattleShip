3415e2a522c24a17e4629ebe8bd650b50ffd51de
alsacchi <andrealorenzosacchi@gmail.com>
2018-05-03 19:56:24 +0200

    Improvement and bug fix

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # PROGETTO BATTAGLIA NAVALE
 Scritto in C
+## Comandi:
+Freccete direzionale per muovere il cursore.
+'C' per posizionare una barca rappresentata da una 'x'. 
+'R' per rimuovere una barca.
+Enter per cambiare player DA P1 A P2 durante la fase di Preparazione.
+'S' per iniziare la battaglia solo quando entrambi i player hanno almeno 5 barche ciascuno.
+'Q' per uscire in un qualsiasi momento.
 
-
-
-alsacchi
+By: alsacchi
--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #define MIN_BOAT 5
 
 void refreshgriglia(WINDOW *wingriglia,char (*griglia)[10]);
+void printmain(int y, int x, char *mes);
 int main(int argc, char *argv[]) {
 	setlocale(LC_ALL, "");
 	//Variabili
@@ -71,6 +72,7 @@ int main(int argc, char *argv[]) {
 	int player_1_score = 0;
 	int player_2_score = 0;
 	bool started = false;
+	char *player;
 	WINDOW *wingriglia, *winmove, *winscore, *lettere;
 	//Inizializzo le finestre
 	initscr();
@@ -84,12 +86,14 @@ int main(int argc, char *argv[]) {
 	keypad(winmove, TRUE);
 	griglia = griglia_p1;
 	//Inizio ciclo
-	mvwprintw(lettere, 0, 2, "ABCDEFGJKL");
+	mvwprintw(lettere, 0, 2, "ABCDEFGHIJ");
 	mvwprintw(lettere, 2, 0, "1\n2\n3\n4\n5\n6\n7\n8\n9\n0");
 	wrefresh(lettere);
 	while (true) {
 		//clear();
 		//Disegno i contorni di wingriglia
+		mvwprintw(lettere, 0, 2, "ABCDEFGHIJ");
+		mvwprintw(lettere, 2, 0, "1\n2\n3\n4\n5\n6\n7\n8\n9\n0");
 		box(wingriglia, 0, 0);
 		box(winscore, 0, 0);
 		//Stampo lo score
@@ -98,10 +102,18 @@ int main(int argc, char *argv[]) {
 		} else {
 			mvwprintw(winscore, 1, 1, "BATTLE");
 		}
+		switch (turn) {
+			case 0:
+				player = "P1";
+				break;
+			case 1:
+				player = "P2";
+				break;
+		}
 		mvwprintw(winscore, 2, 1, "Boats-P1: %d", boat_plc_p1);
 		mvwprintw(winscore, 3, 1, "Boats-P2: %d", boat_plc_p2);
 		mvwprintw(winscore, 4, 1, "MAX-BOAT: %d", MAX_BOAT);
-		mvwprintw(winscore, 5, 1, "Player TURN: %d", turn);
+		mvwprintw(winscore, 5, 1, "Player TURN: %s", player);
 		mvwprintw(winscore, 6, 1, "P1-SCORE: %d", player_1_score);
 		mvwprintw(winscore, 7, 1, "P2-SCORE: %d", player_2_score);
 		//Stampo il cursore Virtuale
@@ -142,60 +154,57 @@ int main(int argc, char *argv[]) {
 				}
 				break;
 			case 's':
+				werase(winscore);
+				if(boat_plc_p1 < MIN_BOAT || boat_plc_p2 < MIN_BOAT) {
+					mvwprintw(winscore, 8, 1, "P2|P1 NOT SET!");
+					break;
+				}
 				started = true;
 				griglia = griglia_p1_shoot;
 				turn = 0;
-				werase(winscore);
 				break;
 			case ' ':
-				werase(winscore);
-				if (turn == 0 && griglia_p2[pos_y-offset][pos_x-offset] == BOAT && started == true) {
+				werase(stdscr);
+				if (turn == 0 && griglia_p2[pos_y-offset][pos_x-offset] == BOAT && started == true && griglia_p1_shoot[pos_y-offset][pos_x-offset] == ' ') {
 					turn = 1;
 					griglia[pos_y-offset][pos_x-offset] = 'C';
 					griglia = griglia_p2_shoot;
 					player_1_score++;
 					boat_plc_p2--;
-					mvwprintw(winscore, 8, 1, "P1 HIT!");
+					printmain(14, 1, "P1 HIT");
+					wrefresh(lettere);
 					break;	
 					
 				}
-				if (turn == 0 && griglia_p2[pos_y-offset][pos_x-offset] == ' ' && started == true) {
+				if (turn == 0 && griglia_p2[pos_y-offset][pos_x-offset] == ' ' && started == true && griglia_p1_shoot[pos_y-offset][pos_x-offset] == ' ') {
 					turn = 1;
 					griglia[pos_y-offset][pos_x-offset] = 'M';
 					griglia = griglia_p2_shoot;
-					mvwprintw(winscore, 8, 1, "P1 MISS!");
+					printmain(14, 1, "P1 MISS");
+					wrefresh(lettere);
 					break;	
 				}
-				if (turn == 1 && griglia_p1[pos_y-offset][pos_x-offset] == BOAT && started == true) {
+				if (turn == 1 && griglia_p1[pos_y-offset][pos_x-offset] == BOAT && started == true && griglia_p2_shoot[pos_y-offset][pos_x-offset] == ' ') {
 					turn = 0;
 					griglia[pos_y-offset][pos_x-offset] = 'C';
 					griglia = griglia_p1_shoot;
 					player_2_score++;
 					boat_plc_p1--;
-					mvwprintw(winscore, 8, 1, "P2 HIT!");
+					printmain(14, 1, "P2 HIT");
+					wrefresh(lettere);
 					break;	
 				}
-				if (turn == 1 && griglia_p1[pos_y-offset][pos_x-offset] == ' ' && started == true) {
+				if (turn == 1 && griglia_p1[pos_y-offset][pos_x-offset] == ' ' && started == true && griglia_p2_shoot[pos_y-offset][pos_x-offset] == ' ') {
 					turn = 0;
 					griglia[pos_y-offset][pos_x-offset] = 'M';
 					griglia = griglia_p1_shoot;
-					mvwprintw(winscore, 8, 1, "P2 MISS!");
+					printmain(14, 1, "P2 MISS");
+					wrefresh(lettere);
 					break;	
 				}
 				break;
 			case 10:
-				if(turn == 1 && started == false && boat_plc_p2 >= MIN_BOAT) {
-					//griglia_p2 = griglia;
-					griglia = griglia_p1;
-					pos_x = 1;
-					pos_y = 1;
-					
-					break;
-				} 
-				if(turn == 1 && started == false && boat_plc_p2 < MIN_BOAT) { 
-					mvwprintw(winscore, 8, 1, "P2 MIN_BOAT!");
-					break;
-				}
+				werase(winscore); 
 				if(turn == 0 && started == false && boat_plc_p1 >= MIN_BOAT) {
 					//griglia_p1 = griglia;
 					griglia = griglia_p2;
@@ -212,6 +221,25 @@ int main(int argc, char *argv[]) {
 			case 'q':
 				endwin();
 				return 0;
+		}
+		//Controllo se un player ha vinto
+		if(started == true && boat_plc_p1 == 0) {
+			werase(wingriglia);
+			werase(winmove);
+			werase(winscore);
+			printmain(0, 0, "HA VINTO IL GIOCATORE P2!");
+			sleep(2);
+			endwin();
+			return 0;
+		}
+		if(started == true && boat_plc_p2 == 0) {
+			werase(wingriglia);
+			werase(winmove);
+			werase(winscore);
+			printmain(0, 0, "HA VINTO IL GIOCATORE P1!");
+			sleep(2);
+			endwin();
+			return 0;
 		}
 		//Controllo della posizione del cursore virtuale
 		//Probabilmente Ottimizabile
@@ -250,4 +278,8 @@ void refreshgriglia(WINDOW *wingriglia,char (*griglia)[10]) {
 		printw("\n");
 		wrefresh(wingriglia);
 	}
+}
+void printmain(int y, int x, char *mes) {
+	mvwprintw(stdscr, y, x, "%s", mes);
+	wrefresh(stdscr);
 }
